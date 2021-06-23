7ced0009703180055755aa7c8c2d0f92e354e301
joonayd <63727069+joonayd@users.noreply.github.com>
2021-06-23 23:50:22 +0500

    uwu

--- restfulapi/routes/api/products.js
+++ restfulapi/routes/api/products.js
@@ -1,25 +1,26 @@
 const express = require("express");
 let router = express.Router();
-var { Product } = require("../../models/product");
 const validateProduct = require("../../middlewares/validateProduct");
-//get all products
+const auth = require("../../middlewares/auth");
+const admin = require("../../middlewares/admin");
+var { Product } = require("../../models/product");
+//get products
 router.get("/", async (req, res) => {
+  console.log(req.user);
   let page = Number(req.query.page ? req.query.page : 1);
   let perPage = Number(req.query.perPage ? req.query.perPage : 10);
   let skipRecords = perPage * (page - 1);
-
   let products = await Product.find().skip(skipRecords).limit(perPage);
-
   return res.send(products);
 });
-//get single product
+//get single products
 router.get("/:id", async (req, res) => {
   try {
     let product = await Product.findById(req.params.id);
-    if (!product) return res.status(400).send("product with given id isnt present"); //when id isnt present in db
-    return res.send(product); //everything OK
+    if (!product) return res.status(400).send("Product With given ID is not present"); //when id is not present id db
+    return res.send(product); //everything is ok
   } catch (err) {
-    return res.status(400).send("invalid ID"); //format of id isnt correct
+    return res.status(400).send("Invalid ID"); // format of id is not correct
   }
 });
 //update a record
@@ -30,14 +31,12 @@ router.put("/:id", validateProduct, async (req, res) => {
   await product.save();
   return res.send(product);
 });
-
-//delete
-router.delete("/:id", async (req, res) => {
+//update a record
+router.delete("/:id", auth, admin, async (req, res) => {
   let product = await Product.findByIdAndDelete(req.params.id);
   return res.send(product);
 });
-
-//insert a record
+//Insert a record
 router.post("/", validateProduct, async (req, res) => {
   let product = new Product();
   product.name = req.body.name;
--- restfulapi/routes/api/users.js
+++ restfulapi/routes/api/users.js
@@ -1,27 +1,27 @@
 const express = require("express");
 let router = express.Router();
+let { User } = require("../../models/user");
 var bcrypt = require("bcryptjs");
 const _ = require("lodash");
-let { User } = require("../../models/user");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 router.post("/register", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send("User with given email already exists");
+  if (user) return res.status(400).send("User with given Email already exist");
   user = new User();
   user.name = req.body.name;
   user.email = req.body.email;
   user.password = req.body.password;
-  let salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
+  await user.generateHashedPassword();
   await user.save();
   return res.send(_.pick(user, ["name", "email"]));
 });
-
 router.post("/login", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send("User not registered");
+  if (!user) return res.status(400).send("User Not Registered");
   let isValid = await bcrypt.compare(req.body.password, user.password);
-  if (!isValid) return res.status(401).send("invalid password");
-  res.send("Logged in successfully");
+  if (!isValid) return res.status(401).send("Invalid Password");
+  let token = jwt.sign({ _id: user._id, name: user.name }, config.get("jwtPrivateKey"));
+  res.send(token);
 });
-
 module.exports = router;
